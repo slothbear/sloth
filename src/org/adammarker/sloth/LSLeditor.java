a1ecd0e9f97d59d593a7481551ad6df70157e760
orion <orion>
2005-02-09 03:48:36 +0000

    editor extension, class, icon

---
+++ src/org/adammarker/sloth/LSLeditor.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Feb 8, 2005
+ */
+package org.adammarker.sloth;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+/**
+ * @author Adam Marker 8Feb2005
+ */
+public class LSLeditor extends TextEditor {
+    // implementation of our LSL editor.
+}
