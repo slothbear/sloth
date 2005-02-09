c45e6a881b9b9b2c50ec98747f5720e70ad8e095
orion <orion>
2005-02-09 05:06:10 +0000

    LSLconstants structure (+ all language and color constants)

--- src/org/adammarker/sloth/Scanner.java
+++ src/org/adammarker/sloth/Scanner.java
@@ -6,27 +6,52 @@ package org.adammarker.sloth;
 import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.rules.EndOfLineRule;
 import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IWhitespaceDetector;
+import org.eclipse.jface.text.rules.IWordDetector;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
+import org.eclipse.jface.text.rules.SingleLineRule;
 import org.eclipse.jface.text.rules.Token;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.jface.text.rules.WhitespaceRule;
+import org.eclipse.jface.text.rules.WordRule;
 
 /**
  * @author Adam Marker  8Feb05
  */
 public class Scanner extends RuleBasedScanner {
+ 
+    final IWordDetector wordDetector =
+        new IWordDetector() {
+		public boolean isWordStart(char c) { return Character.isJavaIdentifierStart(c); }
+		public boolean isWordPart(char c) {	return Character.isJavaIdentifierPart(c); }
+	} ;
 
-    //TODO:  figure out where these declarations should go.
-    static final TextAttribute COMMENT = new TextAttribute(
-            new Color(Display.getCurrent(), new RGB(204,76,39)),
-	        null, SWT.NONE);
-    
-    public Scanner() {
-        setRules(new IRule[] {	
-				new EndOfLineRule("//", new Token(COMMENT)),
-				}) ;
-    }
+		public Scanner() {
+			setRules(new IRule[] {
+//TODO:  type and string colors very close
+//TODO:  constant and event colors very close
+			    listRule(LSLconstants.KEYWORDS, LSLconstants.KEYWORD),
+			    listRule(LSLconstants.TYPES, LSLconstants.TYPE),
+			    listRule(LSLconstants.EVENTS, LSLconstants.EVENT),
+			    listRule(LSLconstants.CONSTANTS, LSLconstants.CONSTANT),
+			    listRule(LSLconstants.FUNCTIONS, LSLconstants.FUNCTION),
+	
+				new EndOfLineRule("//", new Token(LSLconstants.COMMENT)),
+				new SingleLineRule("\"", "\"", new Token(LSLconstants.STRING), '\\'),
+//TODO:  is single-quote style string allowed?
+				new SingleLineRule("'", "'", new Token(LSLconstants.STRING), '\\'),
+//TODO:  what does the whitespacerule do for me?
+				new WhitespaceRule(new IWhitespaceDetector() {
+					public boolean isWhitespace(char c) { return Character.isWhitespace(c);	}
+				}),
+			});
+		}
+
+	    private WordRule listRule(String[] strings, TextAttribute style) {        
+	        WordRule rule = new WordRule(wordDetector);
+	        Token tok = new Token(style) ;
+			for (int n = 0; n < strings.length; n++)
+				rule.addWord(strings[n], tok);
+	        return rule ;
+	    }
 
 }
